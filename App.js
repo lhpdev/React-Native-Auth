168aa5d5593bb73013e308ae018b932838843230
Lucas <lucass.hauptmann@gmail.com>
2019-04-25 17:01:53 -0300

    Finish the App development: * Add firebase to project * Import common files from the other project * Update App.js to identify when user is loggedin or not

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Header } from './src/components/common/Header';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner, CardSection } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
 
-export default class App extends React.Component {
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAnw_6iGvsYO28LduqbYWqmTxGM1uVthps",
+      authDomain: "auth-8f7ce.firebaseapp.com",
+      databaseURL: "https://auth-8f7ce.firebaseio.com",
+      projectId: "auth-8f7ce",
+      storageBucket: "auth-8f7ce.appspot.com",
+      messagingSenderId: "117047649322"
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <CardSection>
+            <Spinner size="large" />
+          </CardSection>
+        );
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>Open up App.js to start working on your app!</Text>
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+export default App;
